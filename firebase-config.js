96d758c55fc1ae531defc27caae4de917b04c3d6
Law Fifteen <MorganAmbrose@proton.me>
2026-07-21 15:13:57 -0600

    Remove firebase-config.js from tracking (credentials kept local only)

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDNQj4LyBPMQoLRIiwWZXt-buwPKsorQDI",
-  authDomain: "lawfifteentwitch.firebaseapp.com",
-  databaseURL: "https://lawfifteentwitch-default-rtdb.firebaseio.com",
-  projectId: "lawfifteentwitch",
-  storageBucket: "lawfifteentwitch.firebasestorage.app",
-  messagingSenderId: "147413859896",
-  appId: "1:147413859896:web:37d1efaff5c4cecbf8b0cd"
-};
